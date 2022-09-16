212e34a6b5c9832adf9652287688cec9abd32635
jycouet <jycouet@gmail.com>
2022-09-16 23:48:57 +0200

    :construction: UPDATE: integration to explore ideas!

---
+++ integration/src/routes/query-param/user-[userId]/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { browser } from '$app/environment';
+  import { GQL_Page_User } from '$houdini';
+  import Loading from '../Loading.svelte';
+
+  // TODO
+  //   +layout.gql ???
+  //   import type { LayoutData } from './$houdini'; ???
+
+  // import type { LayoutData } from './$types';
+  // export let data: LayoutData;
+  // $: ({ Page_User } = data);
+
+  // TODO: For what ever reason, sometimes in client side navigation, I get 2 Page_User query... But not always!
+
+  $: browser && GQL_Page_User.fetch({ variables: { userId: $page.params['userId'] } });
+
+  function TabLinkKeepingContext(id: string, url: URL) {
+    const parts = url.pathname.split('/');
+    return `../user-${id}/${parts[parts.length - 1]}?${url.searchParams.toString()}`;
+  }
+</script>
+
+Pages 👉
+<a href={TabLinkKeepingContext('1', $page.url)}>user-1</a>
+<a href={TabLinkKeepingContext('2', $page.url)}>user-2</a>
+<a href={TabLinkKeepingContext('3', $page.url)}>user-3</a>
+
+<hr />
+<h3>Page User</h3>
+{#if $GQL_Page_User.data}
+  <div id="result">
+    {$GQL_Page_User.data?.user.name}
+  </div>
+{:else}
+  <Loading />
+{/if}
+
+<hr />
+
+Tabs 👉
+<a href="./birth">Birth</a>
+<a href="./fiends">Fiends</a>
+<a href="./fiends?size=3">Fiends (top 3)</a>
+
+<hr />
+
+<slot />
---
+++ integration/src/routes/query-param/user-[userId]/birth/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Loading from '../../Loading.svelte';
+  import type { PageData } from './$houdini';
+
+  export let data: PageData;
+
+  $: ({ Page_User_Birth } = data);
+</script>
+
+<h4>Birth</h4>
+
+<!-- 
+  user.id is already resolved by some other places... we be instant
+  isFetching is maybe a bit wrong
+    - I have a delay of 1 sec and I'm in cacheOrNetwork mode
+    - even if everything is on cache, I get a quick flickering of the loading state
+    - if it hit the cache, isFetching should never go to true.
+-->
+{#if $Page_User_Birth.isFetching}
+  {console.log('isFetching in the console')}
+  <!-- to delete... -->
+  <Loading />
+  <Loading />
+  <Loading />
+  <Loading />
+  <Loading />
+  <Loading />
+{/if}
+{#if $Page_User_Birth.data?.user.birthDate}
+  {$Page_User_Birth.data?.user.birthDate?.toISOString().split('T')[0]}
+{:else}
+  <Loading />
+{/if}
